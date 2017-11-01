1d2c20490a626320876a19f98e97f91945c02e08
asunciod <asunciod@oregonstate.edu>
2017-10-31 18:10:13 -0700

    upload assignment-4 files

---
+++ projects/asunciod/dominion/randomtestadventurer.c
@@ -0,0 +1,94 @@
+/* -----------------------------------------------------------------------
+* David Asuncion
+* CS 362-400
+* Assignment 4 - randomtestadventurer.c
+* testing card: adventurer
+*
+* randomtestadventurer: randomtestadventurer.c dominion.o rngs.o
+*      gcc -o randomtestadventurer -g  randomtestadventurer.c dominion.o rngs.o $(CFLAGS)
+* -----------------------------------------------------------------------
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "card_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTCARD "adventurer"
+
+int main() {
+
+	int i, j;
+	int seed = 1000;
+	int numPlayers = 2;
+	int temphand[MAX_HAND];
+	struct gameState G, testG;
+	int k[10] = { adventurer, embargo, village, minion, mine, cutpurse,
+		sea_hag, tribute, smithy, council_room };
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+
+	printf("----------------- Testing Card: %s ----------------\n\n", TESTCARD);
+
+	// ----------- TEST 1: check return value if drawnTreasure parameter is varied --------------
+	printf("TEST 1: testing drawnTreasure parameter < 1\n");
+
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+	testG.hand[0][0] = adventurer;
+
+	int min = 2;
+	int max = 4;
+
+	for (i = 0; i < 10; i++) {
+
+		//range of players between 2-4
+		numPlayers = (min + (rand() % (int)(max - min + 1)));
+
+		// initialize a game state and player cards
+		initializeGame(numPlayers, k, seed, &G);
+
+		// copy the game state to a test case
+		memcpy(&testG, &G, sizeof(struct gameState));
+
+
+		for (j = 0; j < numPlayers; j++) {
+			
+			adventurerCard(0, &testG, j, 0, temphand, 0);
+
+			int countTreasureCards = 0;
+
+			//count how many treasure cards are in hand for randomly selected player
+			for (int k = 0; k < 5; k++) {
+				int card_val = testG.hand[j][k];
+				if (card_val == 4 || card_val == 5 || card_val == 6) {
+					countTreasureCards++;
+				}
+			}
+			
+			printf("number of treasure cards expected > %d, actual = %d\n", 2, countTreasureCards);
+			if (countTreasureCards >= 2) {
+				printf("TEST PASS: SUCCESS...\n");
+			}
+			else {
+				printf("TEST PASS: FAILED...\n");
+			}
+			
+			endTurn(&testG);
+		}
+
+
+	}
+
+	printf("\n\n >>>>> Testing complete for %s <<<<<\n\n", TESTCARD);
+
+
+	return 0;
+}
+
+
---
+++ projects/asunciod/dominion/randomtestcard1.c
@@ -0,0 +1,84 @@
+/* -----------------------------------------------------------------------
+* David Asuncion
+* CS 362-400
+* Assignment 4 - randomtestcard1.c
+* testing card: outpost
+*
+* randomtestcard1: randomtestcard1.c dominion.o rngs.o
+*      gcc -o randomtestcard1 -g  randomtestcard1.c dominion.o rngs.o $(CFLAGS)
+* -----------------------------------------------------------------------
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "card_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1
+#define TESTCARD "outpost"
+
+int main() {
+	int seed = 1000;
+	int randomHandPos;
+	int i, j;
+	int k[10] = { adventurer, council_room, feast, gardens, mine
+		, remodel, smithy, village, baron, great_hall };
+	struct gameState G, testG;
+	int min = 2;
+	int max = 4;
+	int numPlayers;
+
+	printf("----------------- Testing Card: %s ----------------\n\n", TESTCARD);
+
+	for (i = 0; i < 10; i++) {
+
+		//range of players between 2-4
+		numPlayers = (min + (rand() % (int)(max - min + 1)));
+
+		// initialize a game state and player cards
+		initializeGame(numPlayers, k, seed, &G);
+
+		// copy the game state to a test case
+		memcpy(&testG, &G, sizeof(struct gameState));
+
+
+		for (j = 0; j < numPlayers; j++) {
+
+			int cur = whoseTurn(&testG);
+			randomHandPos = rand() % 5;	//randomly select hand position
+			testG.hand[cur][randomHandPos] = outpost;
+
+			outpostCard(&testG, randomHandPos, cur);
+
+			//check outpostplayed flag
+			printf("TEST: Checking outpostPlayed flag for current player\n");
+			printf("outpost played flag expected = %d, actual = %d\n", 1, testG.outpostPlayed);
+			if (testG.outpostPlayed == 0) {
+				printf("TEST PASS: FAILED...\n\n");
+			}
+
+			//check discarded card
+			printf("TEST: Checking played cards for current player\n");
+			printf("expected card in playedCards = %d, actual = %d\n", 23, testG.playedCards[0]);
+			if (testG.playedCards[0] == 23) {
+				printf("TEST PASS: SUCCESS...\n\n");
+			}
+			else {
+				printf("TEST PASS: FAILED...\n\n");
+			}
+			
+			endTurn(&testG);
+		}
+
+
+	}
+
+	printf("\n\n >>>>>Testing complete for %s <<<<<\n\n", TESTCARD);
+
+	return 0;
+}
